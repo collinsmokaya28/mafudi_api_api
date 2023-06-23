a5ec24dfa56313f26c33fdc3e84cdd2be24b9830
+ <+>
2023-06-23 23:17:18 +0300

    updated ApplicationController for errors and exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActionController::ParameterMissing, with: :parameter_missing
+  
+    private
+  
+    def record_not_found(exception)
+      render json: { error: exception.message }, status: :not_found
+    end
+  
+    def parameter_missing(exception)
+      render json: { error: exception.message }, status: :unprocessable_entity
+    end
+  end
+  
