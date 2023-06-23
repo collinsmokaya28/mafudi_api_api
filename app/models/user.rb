1f436752e970dfb29fe63cb8ee81fc13d7626680
+ <+>
2023-06-23 23:09:53 +0300

    updated user model for validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,9 @@ class User < ApplicationRecord
     def to_token_payload
         { sub: id }
     end
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: {minimum: 6}
 end
   
